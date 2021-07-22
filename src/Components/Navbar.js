71a849d4d5904fe59bad9cecba54aff6ab7d5b62
Tomas <tomas.vallejos12@gmail.com>
2021-07-22 12:34:24 -0400

    [About] img size

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,18 +15,20 @@ class Navbar extends Component {
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
             Show navigation
           </a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">
+          <a className="mobile-btn" href="#about" title="Hide navigation">
             Hide navigation
           </a>
 
           <ul id="nav" className="nav">
-            <li className="current">
+            {/*
+            <li>
               <a className="smoothscroll" href="#home">
                 Home
               </a>
             </li>
+            */}
 
-            <li>
+            <li className="current">
               <a className="smoothscroll" href="#about">
                 About
               </a>
